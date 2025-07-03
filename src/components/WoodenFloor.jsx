39cbd988473b57b28a063c2ea533b7fcc8b8e441
MuhammadFaizanIbrahim <mfaizanibrahim26@gmail.com>
2025-07-03 17:44:10 +0500

    2nd Commit by Faizan (03-07-25)

--- src/components/WoodenFloor.jsx
+++ src/components/WoodenFloor.jsx
@@ -3,10 +3,10 @@ import { useTexture } from "@react-three/drei";
 
 const WoodenFloor = () => {
   const woodTextures = useTexture({
-    map: "/textures/wood1/laminate_floor_02_diff_4k.jpg",
-    normalMap: "/textures/wood1/laminate_floor_02_nor_gl_4k.jpg",
-    roughnessMap: "/textures/wood1/laminate_floor_02_rough_4k.jpg",
-    displacementMap: "/textures/wood1/laminate_floor_02_disp_4k.png",
+    map: "/textures/wood/laminate_floor_02_diff_1k.jpg",
+    normalMap: "/textures/wood/laminate_floor_02_nor_gl_1k.jpg",
+    roughnessMap: "/textures/wood/laminate_floor_02_rough_1k.jpg",
+    displacementMap: "/textures/wood/laminate_floor_02_disp_1k.png",
   });
 
   return (
